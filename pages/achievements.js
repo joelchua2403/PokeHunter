a1261ec40dee801d7207a2db28d6f3ebe746c642
Joel Chua <joel.chua.mingyi@gmail.com>
2023-08-14 14:14:46 +0800

    added styling to pokedex page

--- pages/achievements.js
+++ pages/achievements.js
@@ -36,12 +36,16 @@ export default function Achievements() {
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item, index }) => (
                     <View style={[styles.pokemonItem, { width, height }]}>
-                        <Text style={styles.text}>{index + 1}. {item.name}</Text>
+                        <View style={styles.header}>
+                            <Text style={styles.text}>{index + 1}. {item.name.toUpperCase()}</Text>
+                        </View>
                         <Image source={{ uri: item.image }} style={styles.pokemonImage} />
-                        <Text style={styles.details}>Types: {item.types}</Text>
-                        <Text style={styles.details}>Abilities: {item.abilities}</Text>
-                        <Text style={styles.details}>Height: {item.height}</Text>
-                        <Text style={styles.details}>Weight: {item.weight}</Text>
+                        <View style={styles.detailsContainer}>
+                            <Text style={styles.details}>Types: {item.types}</Text>
+                            <Text style={styles.details}>Abilities: {item.abilities}</Text>
+                            <Text style={styles.details}>Height: {item.height}</Text>
+                            <Text style={styles.details}>Weight: {item.weight}</Text>
+                        </View>
                     </View>
                 )}
                 horizontal // Enable horizontal scrolling
@@ -54,25 +58,46 @@ export default function Achievements() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#f0f0f0',
+    },
+    header: {
+        backgroundColor: '#cc0000',
+        padding: 10,
         alignItems: 'center',
-        justifyContent: 'center',
     },
     text: {
-        fontSize: 18,
+        fontSize: 22,
         fontWeight: 'bold',
-        marginBottom: 10,
-        textAlign: 'center',
+        color: 'white',
     },
     pokemonItem: {
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
+        padding: 16,
     },
     pokemonImage: {
-        width: '70%', // You can adjust these as needed
-        height: '50%',
+        width: '60%',
+        height: '40%',
+        borderRadius: 10,
+    },
+    detailsContainer: {
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        padding: 16,
+        marginTop: 16,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     details: {
         fontSize: 14,
-        color: 'gray',
+        color: 'black',
+        marginBottom: 8,
     },
 });
