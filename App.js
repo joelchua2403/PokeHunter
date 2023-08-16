4f6b9bb372a4adada5294e1bf67d308fd030d76b
Joel Chua <joel.chua.mingyi@gmail.com>
2023-08-16 15:55:22 +0800

    add tab icons

--- App.js
+++ App.js
@@ -4,13 +4,12 @@ import { ApiProvider } from "./context/apiContext";
 import Home from "./pages/home";
 import Play from "./pages/play";
 import Achievements from "./pages/achievements";
-
 import NotificationSettings from "./pages/NotificationSettings";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SliderProvider from "./Slider/SliderProvider";
-
+import Icon from "react-native-vector-icons/Ionicons";
 import { GyroProvider } from "./context/gyroContext";
 
 export default function App() {
@@ -26,10 +25,34 @@ export default function App() {
 
         <NavigationContainer>
           <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Play" component={Play} />
-            <Tab.Screen name="PokeDex" component={Achievements} />
-            <Tab.Screen name="Settings" component={NotificationSettings} />
+            <Tab.Screen name="Home" component={Home}  
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="home-outline" color={color} size={size} />
+              ),
+            }}
+            />
+            <Tab.Screen name="Play" component={Play} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="game-controller-outline" color={color} size={size} />
+              ),
+            }}
+            />
+            <Tab.Screen name="PokeDex" component={Achievements} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="book-outline" color={color} size={size} />
+              ),
+            }}
+            />
+            <Tab.Screen name="Settings" component={NotificationSettings} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="settings-outline" color={color} size={size} />
+              ),
+            }}
+            />
           </Tab.Navigator>
         </NavigationContainer>
 
